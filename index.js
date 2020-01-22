3ed57aed09b3e144671b05b34842fb6ff54f0311
Cees Wang <wangcees@gmail.com>
2020-01-22 16:27:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-function bfs(rootNode, vertices, edges){
+function bfs(rootNode, vertices, edges) {
+    let array = [];
+    let queue = [];
+    rootNode.distance = 0;
+    queue.push(rootNode);
+    while (queue && queue.length) {
+        let first = queue.shift();
+        console.log("IJWEFIJWEF",first.name);
+        array.push(first.name);
+        let adjacentNodes = findAdjacent(first.name, vertices, edges);
+        markDistanceAndPredecessor(first, adjacentNodes);
+        queue.push(...adjacentNodes);        
+    }
+    return array.replace(/'/g, '"');;
+}
 
+function findAdjacent(node, vertices, edges) {
+    let adjacentEdgePairs = [];
+    edges.forEach(edgePair => {
+        edgePair.forEach(edge => {
+            if (edge === node) {
+                adjacentEdgePairs.push(...edgePair);
+            }
+        });
+    });
+    let adjacentEdges = adjacentEdgePairs.filter(edge => edge !== node);
+    let adjacentNodes = [];
+    adjacentEdges.forEach(edge => {
+        vertices.forEach(vertice => {
+            if (vertice.name === edge && vertice.distance === null) {
+                adjacentNodes.push(vertice);
+            }
+        });
+    });
+    return adjacentNodes;
+}
+
+function markDistanceAndPredecessor(vertice, adjacentNodes) {
+    adjacentNodes.forEach(node => {
+        node.distance = vertice.distance + 1;
+        node.predecessor = vertice;
+    });
 }
